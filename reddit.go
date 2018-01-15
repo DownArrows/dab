2abdc42b2b50967c85007a728cb2e5dbfe8a98eb
Jacques Derrida <jacques.derrida@posteo.de>
2018-01-15 23:00:43 +0100

    Improve reddit client mockability and add UserExists

--- reddit.go
+++ reddit.go
@@ -3,47 +3,47 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"strings"
 	"strconv"
+	"strings"
 	"time"
 )
 
-
 const access_token_url = "https://www.reddit.com/api/v1/access_token"
-
 
 const request_base_url = "https://oauth.reddit.com"
 
-
 type OAuthResponse struct {
-	Token string `json:"access_token"`
+	Token   string `json:"access_token"`
 	Refresh string `json:"refresh_token"`
-	Type string `json:"token_type"`
-	Timeout int `json:"expires_in"`
-	Scope string `json:"scope"`
+	Type    string `json:"token_type"`
+	Timeout int    `json:"expires_in"`
+	Scope   string `json:"scope"`
 }
-
 
 type RedditAuth struct {
 	Username string
 	Password string
-	Id string
-	Key string
+	Id       string
+	Key      string
 }
 
+type RedditScanner interface {
+	UserExists(username string) (bool, error)
+	UserComments(username string, position string) ([]Comment, string, error)
+}
 
 type RedditClient struct {
-	Client *http.Client
+	Client  *http.Client
 	Version string
-	OAuth OAuthResponse
-	Auth RedditAuth
-	ticker *time.Ticker
+	OAuth   OAuthResponse
+	Auth    RedditAuth
+	ticker  *time.Ticker
 }
-
 
 type userComments struct {
 	Data struct {
@@ -54,12 +54,12 @@ type userComments struct {
 	}
 }
 
-func NewRedditClient(auth RedditAuth) (*RedditClient, error) {
+func NewRedditClient(auth RedditAuth) (*RedditScanner, error) {
 	http_client := &http.Client{}
 	var client = &RedditClient{
-		Client: http_client,
+		Client:  http_client,
 		Version: "0.1.0",
-		ticker: time.NewTicker(time.Second),
+		ticker:  time.NewTicker(time.Second),
 	}
 
 	err := client.Connect(auth)
@@ -79,8 +79,8 @@ func (rc *RedditClient) Connect(auth RedditAuth) error {
 
 	var auth_conf = url.Values{
 		"grant_type": {"password"},
-		"username": {rc.Auth.Username},
-		"password": {rc.Auth.Password}}
+		"username":   {rc.Auth.Username},
+		"password":   {rc.Auth.Password}}
 	auth_form := strings.NewReader(auth_conf.Encode())
 
 	req, _ := http.NewRequest("POST", access_token_url, auth_form)
@@ -102,9 +102,9 @@ func (rc *RedditClient) Connect(auth RedditAuth) error {
 	return err
 }
 
-func (rc *RedditClient) PrepareRequest(req *http.Request) *http.Request{
+func (rc *RedditClient) PrepareRequest(req *http.Request) *http.Request {
 	req.Header.Set("User-Agent", rc.UserAgent())
-	req.Header.Set("Authorization", "bearer " + rc.OAuth.Token)
+	req.Header.Set("Authorization", "bearer "+rc.OAuth.Token)
 	return req
 }
 
@@ -112,11 +112,11 @@ func (rc *RedditClient) Do(req *http.Request) (*http.Response, error) {
 	return rc.Client.Do(req)
 }
 
-func (rc *RedditClient) RawRequest(verb string, path string, data io.Reader) ([]byte, error) {
+func (rc *RedditClient) RawRequest(verb string, path string, data io.Reader) ([]byte, int, error) {
 
 	<-rc.ticker.C
 
-	req, err := http.NewRequest(verb, request_base_url + path, data)
+	req, err := http.NewRequest(verb, request_base_url+path, data)
 	if err != nil {
 		return nil, err
 	}
@@ -135,32 +135,35 @@ func (rc *RedditClient) RawRequest(verb string, path string, data io.Reader) ([]
 		return rc.RawRequest(verb, path, data)
 	}
 
-	if res.StatusCode != 200 {
-		return nil, errors.New("bad status code: " + strconv.Itoa(res.StatusCode))
-	}
-
 	raw_data, read_err := ioutil.ReadAll(res.Body)
 	if read_err != nil {
-		return nil, read_err
+		return nil, status, read_err
 	}
 
-	return raw_data, nil
+	return raw_data, res.StatusCode, nil
 }
 
-func (rc *RedditClient) FetchComments(username string, after string) ([]Comment, string, error){
+func (rc *RedditClient) UserComments(username string, position string) ([]Comment, string, error) {
 	params := "?sort=new&limit=100"
 	if after != "" {
-		params += "&after=" + after
+		params += "&after=" + position
 	}
-	res, err := rc.RawRequest("GET", "/u/" + username + params, nil)
+
+	res, status, err := rc.RawRequest("GET", "/u/"+username+params, nil)
+
 	if err != nil {
-		return nil, "", err
+		return nil, position, err
+	}
+	if status != 200 {
+		template := "Bad response status when fetching the comments of %s: %d"
+		msg := fmt.Sprintf(template, username, status)
+		return nil, position, errors.New(msg)
 	}
 
 	parsed := &userComments{}
 	err = json.Unmarshal(res, parsed)
 	if err != nil {
-		return nil, "", err
+		return nil, position, err
 	}
 
 	children := parsed.Data.Children
@@ -169,5 +172,24 @@ func (rc *RedditClient) FetchComments(username string, after string) ([]Comment,
 		comments[i] = child.Data
 	}
 
-	return comments, parsed.Data.After, nil
+	new_position := parsed.Data.After
+	return comments, new_position, nil
+}
+
+func (rc *RedditClient) UserExists(username string) (bool, error) {
+	_, status, err := rc.RawRequest("GET", "/u/"+username, nil)
+
+	var exists bool
+
+	if status == 404 {
+		exists = false
+	} else if status == 200 {
+		exists = true
+	} else {
+		template := "Bad response status when looking up %s: %d"
+		msg := fmt.Sprintf(template, username, status)
+		err = errors.New(msg)
+	}
+
+	return exists, err
 }
