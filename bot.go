647bd8538d0efd469bf8ef8e1e72034cdcba1e59
Jacques Derrida <jacques.derrida@posteo.de>
2018-01-21 10:41:16 +0100

    Fix the regex that checks usernames

--- bot.go
+++ bot.go
@@ -174,14 +174,14 @@ func (bot *Bot) AddUserServer(queries chan UserAddition) {
 func (bot *Bot) AddUser(username string, hidden bool) (bool, error) {
 	bot.logger.Print("Trying to add user ", username)
 
-	sane, err := regexp.MatchString(`\w+`, username)
+	sane, err := regexp.MatchString(`^[[:word:]-]+$`, username)
 	if err != nil {
 		return false, err
 	}
 
 	if !sane {
-		msg := "username \"%s\" contains forbidden characters or is empty"
-		return false, errors.New(fmt.Sprintf(msg, username))
+		msg := "username contains forbidden characters or is empty"
+		return false, errors.New(msg)
 	}
 
 	has_user, err := bot.HasUser(username)
