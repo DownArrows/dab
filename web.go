a44b7f8f18c4056c1ecada1f2a98c9e3c4641e02
Jacques Derrida <jacques.derrida@posteo.de>
2018-10-07 15:49:59 +0200

    [web] refactor and use redirects for current and last week reports

--- web.go
+++ web.go
@@ -9,23 +9,22 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
-	"time"
 )
-
-type WebServer struct {
-	Server          *http.Server
-	Reports         ReportFactory
-	markdownOptions blackfriday.Option
-}
-
-type WebServerError struct {
-	Status int
-	Error  error
-}
 
 type Response struct {
 	Actual http.ResponseWriter
 	Gzip   *gzip.Writer
+}
+
+func NewResponse(w http.ResponseWriter, r *http.Request) Response {
+	if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+		w.Header().Set("Content-Encoding", "gzip")
+		return Response{
+			Actual: w,
+			Gzip:   gzip.NewWriter(w),
+		}
+	}
+	return Response{Actual: w}
 }
 
 func (r Response) Write(data []byte) (int, error) {
@@ -42,15 +41,15 @@ func (r Response) Close() error {
 	return r.Gzip.Close()
 }
 
-func NewResponse(w http.ResponseWriter, r *http.Request) Response {
-	if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
-		w.Header().Set("Content-Encoding", "gzip")
-		return Response{
-			Actual: w,
-			Gzip:   gzip.NewWriter(w),
-		}
-	}
-	return Response{Actual: w}
+type HTMLReportComment struct {
+	ReportComment
+	HTMLBody template.HTML
+}
+
+type WebServer struct {
+	Server          *http.Server
+	Reports         ReportFactory
+	markdownOptions blackfriday.Option
 }
 
 func NewWebServer(listen string, reports ReportFactory) *WebServer {
@@ -68,13 +67,7 @@ func NewWebServer(listen string, reports ReportFactory) *WebServer {
 	mux.HandleFunc("/reports/lastweek", wsrv.ReportLatest)
 	mux.HandleFunc("/reports/source/", wsrv.ReportSource)
 
-	wsrv.Server = &http.Server{
-		Addr:           listen,
-		Handler:        mux,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-	}
+	wsrv.Server = &http.Server{Addr: listen, Handler: mux}
 
 	return wsrv
 }
@@ -87,79 +80,55 @@ func (wsrv *WebServer) Close() error {
 	return wsrv.Server.Close()
 }
 
-func (wsrv *WebServer) newError(status int, err error) *WebServerError {
-	return &WebServerError{Status: status, Error: err}
-}
-
-func (wsrv *WebServer) processError(w http.ResponseWriter, err *WebServerError) {
-	http.Error(w, fmt.Sprint(err.Error), err.Status)
-}
-
 func (wsrv *WebServer) ReportIndex(w http.ResponseWriter, r *http.Request) {
 	http.NotFound(w, r)
 }
 
 func (wsrv *WebServer) ReportSource(w http.ResponseWriter, r *http.Request) {
-	if report := wsrv.getReportFromURL("/reports/source/", w, r); report.Len() != 0 {
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		output := NewResponse(w, r)
-		defer output.Close()
-		autopanic(WriteMarkdownReport(report, output))
+	week, year, err := weekAndYear(ignoreTrailing(subPath("/reports/source/", r)))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
+
+	report := wsrv.Reports.ReportWeek(week, year)
+	if report.Len() == 0 {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	output := NewResponse(w, r)
+	defer output.Close()
+	autopanic(WriteMarkdownReport(report, output))
 }
 
 func (wsrv *WebServer) Report(w http.ResponseWriter, r *http.Request) {
-	report := wsrv.getReportFromURL("/reports/", w, r)
-	if report.Len() == 0 {
-		return
+	week, year, err := weekAndYear(ignoreTrailing(subPath("/reports/", r)))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	} else {
+		report := wsrv.Reports.ReportWeek(week, year)
+		wsrv.serveHTMLReport(report, w, r)
 	}
-	write_page := wsrv.prepareReportPage(report)
-	write_page(w, r)
 }
 
 func (wsrv *WebServer) ReportCurrent(w http.ResponseWriter, r *http.Request) {
 	week, year := wsrv.Reports.CurrentWeekCoordinates()
-	report, err := wsrv.getReport(week, year)
-	if err != nil {
-		wsrv.processError(w, err)
-		return
-	}
-	write_page := wsrv.prepareReportPage(report)
-	write_page(w, r)
+	redirectToReport(week, year, w, r)
 }
 
 func (wsrv *WebServer) ReportLatest(w http.ResponseWriter, r *http.Request) {
 	week, year := wsrv.Reports.LastWeekCoordinates()
-	report, err := wsrv.getReport(week, year)
-	if err != nil {
-		wsrv.processError(w, err)
+	redirectToReport(week, year, w, r)
+}
+
+func (wsrv *WebServer) serveHTMLReport(report Report, w http.ResponseWriter, r *http.Request) {
+	if report.Len() == 0 {
+		http.NotFound(w, r)
 		return
 	}
-	write_page := wsrv.prepareReportPage(report)
-	write_page(w, r)
-}
 
-func (wsrv *WebServer) getReportFromURL(prefix string, w http.ResponseWriter, r *http.Request) Report {
-	var report Report
-	week, year, ws_err := wsrv.getWeekYearFromURL(r, prefix)
-	if ws_err != nil {
-		wsrv.processError(w, ws_err)
-		return report
-	}
-	report, ws_err = wsrv.getReport(week, year)
-	if ws_err != nil {
-		wsrv.processError(w, ws_err)
-		return report
-	}
-	return report
-}
-
-type HTMLReportComment struct {
-	ReportComment
-	HTMLBody template.HTML
-}
-
-func (wsrv *WebServer) prepareReportPage(report Report) func(http.ResponseWriter, *http.Request) {
 	var comments []HTMLReportComment
 	for _, src := range report.Comments() {
 		var comment HTMLReportComment
@@ -175,29 +144,14 @@ func (wsrv *WebServer) prepareReportPage(report Report) func(http.ResponseWriter
 		"Comments": comments,
 	}
 
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html")
-		output := NewResponse(w, r)
-		defer output.Close()
-		autopanic(HTMLReportPage.Execute(output, data))
-	}
+	w.Header().Set("Content-Type", "text/html")
+	output := NewResponse(w, r)
+	defer output.Close()
+	autopanic(HTMLReportPage.Execute(output, data))
 }
 
-func (wsrv *WebServer) getReport(week uint8, year int) (Report, *WebServerError) {
-	report := wsrv.Reports.ReportWeek(week, year)
-	if report.Len() == 0 {
-		return report, wsrv.newError(404, fmt.Errorf("No report for week %d year %d.", week, year))
-	}
-	return report, nil
-}
-
-func (wsrv *WebServer) getWeekYearFromURL(r *http.Request, leadingPath string) (uint8, int, *WebServerError) {
-	path := subPath(leadingPath, r)
-	week, year, err := weekAndYear(path)
-	if err != nil {
-		return 0, 0, wsrv.newError(400, err)
-	}
-	return week, year, nil
+func redirectToReport(week uint8, year int, w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, fmt.Sprintf("/reports/%d/%d", year, week), http.StatusTemporaryRedirect)
 }
 
 func subPath(prefix string, r *http.Request) []string {
