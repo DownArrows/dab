f09756bef083ad1fac1d2cac57dcfa5273c740e8
Jacques Derrida <jacques.derrida@posteo.de>
2018-10-16 05:45:53 +0200

    [main] allow proper exit while launching DAB

--- main.go
+++ main.go
@@ -9,11 +9,26 @@ import (
 
 func main() {
 	dab := NewDownArrowsBot(os.Stderr, log.Lshortfile, os.Stdout)
-	defer dab.Close()
-	dab.Launch(os.Args[1:])
-	if dab.Daemon {
-		sig := make(chan os.Signal, 1)
-		signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
-		<-sig
+	done := make(chan bool)
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				dab.Logger.Print(r)
+				done <- true
+			}
+		}()
+		dab.Launch(os.Args[1:])
+		done <- !dab.Daemon
+	}()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	for is_done := false; !is_done; {
+		select {
+		case <-sig:
+			is_done = true
+		case is_done = <-done:
+		}
 	}
+	dab.Close()
 }
