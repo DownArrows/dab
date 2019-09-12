caee67b1731ebf3baa0294e4357df6edcf3ae4cd
Jacques Derrida <jacques.derrida@posteo.de>
2019-09-12 22:51:04 +0200

    Bump to v1.19.0

--- dab.go
+++ dab.go
@@ -11,7 +11,7 @@ import (
 )
 
 // Version of the application.
-var Version = SemVer{1, 18, 8}
+var Version = SemVer{1, 19, 0}
 
 // DefaultChannelSize is the size of the channels that are used throughout of the application, unless there's a need for a specific size.
 const DefaultChannelSize = 100
