65a8eeebb0706dea0d4dd31ea8c4269ef7f575ec
Jacques Derrida <jacques.derrida@posteo.de>
2020-03-06 13:05:14 +0100

    Add general-purpose TLS configuration from ssl-config.mozilla.org

--- web.go
+++ web.go
@@ -362,7 +362,20 @@ func (wsrv *WebServer) listen() error {
 	defer listener.Close()
 
 	if wsrv.TLS.Enabled() {
-		tls_conf := &tls.Config{Certificates: []tls.Certificate{wsrv.certificate}}
+		tls_conf := &tls.Config{
+			Certificates: []tls.Certificate{wsrv.certificate},
+			MinVersion:   tls.VersionTLS12,
+
+			PreferServerCipherSuites: false,
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			},
+		}
 		listener = tls.NewListener(listener, tls_conf)
 		wsrv.logger.Infof("TLS enabled on %s", wsrv.Listen)
 		defer listener.Close()
