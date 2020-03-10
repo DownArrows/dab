5ddb9562a28e1915cdfc3cd8ce43bd267bd1f9bd
Jacques Derrida <jacques.derrida@posteo.de>
2020-03-10 17:05:31 +0100

    Reduce maintenance burden by not specifying a list of ciphers

--- web.go
+++ web.go
@@ -380,14 +380,6 @@ func (wsrv *WebServer) listen() error {
 			MinVersion:     tls.VersionTLS12,
 
 			PreferServerCipherSuites: false,
-			CipherSuites: []uint16{
-				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-				tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-			},
 		}
 		listener = tls.NewListener(listener, tls_conf)
 		wsrv.logger.Infof("TLS enabled on %s", wsrv.Listen)
